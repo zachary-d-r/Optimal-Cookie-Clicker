d0fa1e6550003b17c166242c89e06f9971a5be93
Zachary Robinson <53249525+zachary-d-r@users.noreply.github.com>
2021-07-25 17:05:10 -0500

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,18 @@
+import time
+
+class Timer:
+    
+    def __init__(self):
+        self.clearTimer()
+
+    # Put this at the begining of the function or whatever code you want to test
+    def codeBegining(self):
+        self.begining = time.time()
+
+    # Put this at the end of whatever code you want to time
+    def codeEnd(self):
+        self.end = time.time()
+
+    # This will return how much time it took to run the code in between codeBegining() and codeEnd()
+    def getCodeTime(self):
+        return self.end - self.begining
